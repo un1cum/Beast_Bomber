2f44a72cce4e2c8c59c3e7452760740d3b4ce2b5
un1cum <80776324+un1cum@users.noreply.github.com>
2023-04-07 19:50:43 +0300

    Add files via upload

--- core/etc/functions.py
+++ core/etc/functions.py
@@ -19,14 +19,15 @@ import re
 import json
 import ipaddress
 import requests
-from beast import *
+import time
+import random
 from bs4 import BeautifulSoup
 from sys import platform
 from colorama import Fore, init
 from fake_useragent import UserAgent
 
 init()
-proxies_file = os.path.abspath('input/proxies.txt')
+
 
 def logo_main():
     text = """
@@ -187,6 +188,7 @@ def validate_ip(ip):
     except ValueError:
         return False
 
+
 def validate_port(port):
     return str(port).isdigit() and 1 <= int(port) <= 65535
 
@@ -196,6 +198,10 @@ def update_proxies():
         js_file = json.load(file)
 
     lang = js_file["language"]
+    urls = ['https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt',
+            'https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt',
+            'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt',
+            'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt']
 
     with open(os.path.abspath('input/proxies.txt'), 'w+') as f:
         f.seek(0)
@@ -208,7 +214,7 @@ def update_proxies():
 
     logo_proxies()
 
-    ua = UserAgent()
+    ua = UserAgent(verify_ssl=False)
     user = ua.random
 
     try:
@@ -216,7 +222,7 @@ def update_proxies():
         soup = BeautifulSoup(res.text, "lxml")
         cnt3 = 0
 
-        with open(os.path.abspath(proxies_file), "a", encoding="utf-8") as file:
+        with open(os.path.abspath('input/proxies.txt'), "a", encoding="utf-8") as file:
             for child in soup.recursiveChildGenerator():
                 if child.name == 'td':
                     if cnt3 == 0:
@@ -234,7 +240,7 @@ def update_proxies():
         res = requests.get('https://hidemy.name/ru/proxy-list', headers={'User-Agent': user})
         soup = BeautifulSoup(res.text, "lxml")
 
-        with open(os.path.abspath(proxies_file), "a", encoding="utf-8") as file:
+        with open(os.path.abspath('input/proxies.txt'), "a", encoding="utf-8") as file:
             for child in soup.recursiveChildGenerator():
                 if child.name == 'td':
                     if validate_ip(child.text):
@@ -243,19 +249,16 @@ def update_proxies():
                         file.write(f"{child.text}\n")
     except requests.exceptions.RequestException as e:
         print(f"Error occurred: {e}")
-    urls = ['https://raw.githubusercontent.com/mertguvencli/http-proxy-list/main/proxy-list/data.txt',
-            'https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt',
-            'https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt',
-            'https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt']
+
     try:
-        with open(os.path.abspath(proxies_file), "w", encoding="utf-8") as file:
+        with open(os.path.abspath('input/proxies.txt'), "w", encoding="utf-8") as file:
             for url in urls:
                 res = requests.get(url, headers={'User-Agent': user})
                 proxy_list = res.text.strip().split('\n')
                 for proxy in proxy_list:
                     if validate_ip(proxy.split(':')[0]) and validate_port(proxy.split(':')[1]):
                         file.write(proxy)
-                        file.write('\n') 
+                        file.write('\n')
     except requests.exceptions.RequestException as e:
         print(f"Error occurred: {e}")
 
@@ -266,3 +269,110 @@ def update_proxies():
 
     time.sleep(1)
 
+
+def get_lang():
+    try:
+        js_file = ''
+        with open(os.path.abspath('core/config.json'), 'r') as file:
+            for line in file:
+                js_file += str(line)
+
+        return json.loads(js_file)["language"]
+    except:
+        return 'en'
+
+
+def get_proxies():
+    proxies = []
+    lang = get_lang()
+
+    try:
+        with open(os.path.abspath('input/proxies.txt'), 'r') as file:
+            for line in file:
+                proxies.append(line.replace('\n', ''))
+    except:
+        if lang == 'ru':
+            print(Fore.RED + '\nОшибка при попытке открыть файл input/proxies.txt')
+        else:
+            print(Fore.RED + '\nError when trying to open a file input/proxies.txt')
+
+    return proxies
+
+
+def generate_email():
+    lib = 'qwertyuiopasdfhgjklzxcvbnm'
+    lib2 = ['@gmail.com', '@hotmail.com', '@yahoo.com', '@yandex.ru']
+    email = ''.join(random.choice(lib) for _ in range(random.randint(10, 25))) + random.choice(lib2)
+    return email
+
+
+def randstr(str_len):
+    lib = '1234567890qwertyuiopasdfghjklzxcvbnm'
+    text = ''.join(random.choices(lib, k=str_len))
+    return text
+
+
+def get_discord_tokens():
+    tokens = []
+    lang = get_lang()
+
+    try:
+        with open(os.path.abspath('input/discord_tokens.txt'), 'r') as file:
+            for line in file:
+                tokens.append(line.replace('\n', ''))
+    except:
+        if lang == 'ru':
+            print(Fore.RED + '\nОшибка при попытке открыть файл input/discord_tokens.txt')
+        else:
+            print(Fore.RED + '\nError when trying to open a file input/discord_tokens.txt')
+
+    return tokens
+
+
+def get_telegram_accounts():
+    lang = get_lang()
+    accounts = []
+
+    try:
+        accounts = os.listdir(os.path.abspath('input/telegram_accounts'))
+    except:
+        if lang == 'ru':
+            print(Fore.RED + '\nОшибка при попытке получить Telegram аккаунты из input/telegram_accounts')
+        else:
+            print(Fore.RED + '\nError when trying to retrieve Telegram accounts from input/telegram_accounts')
+
+    return accounts
+
+
+def get_email_accounts():
+    emails = []
+    lang = get_lang()
+
+    try:
+        with open(os.path.abspath('input/email_accounts.txt'), 'r') as file:
+            for line in file:
+                emails.append(line.replace('\n', ''))
+    except:
+        if lang == 'ru':
+            print(Fore.RED + '\nОшибка при попытке открыть файл input/email_accounts.txt')
+        else:
+            print(Fore.RED + '\nError when trying to open a file input/email_accounts.txt')
+
+    return emails
+
+
+def change_language():
+    lang = get_lang()
+    js_file = os.path.abspath('core/config.json')
+    with open(os.path.abspath(js_file)) as file:
+        js = json.load(file)
+
+    if lang == "ru":
+        js["language"] = "en"
+        with open(os.path.abspath('core/config.json'), 'w') as file:
+            json.dump(js, ensure_ascii=False, indent=4, fp=file)
+
+    else:
+        js["language"] = "ru"
+        with open(os.path.abspath('core/config.json'), 'w') as file:
+            json.dump(js, ensure_ascii=False, indent=4, fp=file)
---
+++ core/etc/settings.py
@@ -0,0 +1,100 @@
+import os
+import fade
+import time
+import ctypes
+from sys import platform
+from colorama import Fore, Style, Back, init
+from core.etc.functions import get_lang, logo_settings, settings_menu_en, settings_menu_ru, update_proxies, change_language
+
+init()
+
+
+class Settings:
+    def __init__(self):
+        self.lang = get_lang()
+
+    def settings_main(self):
+        if platform == 'win32':
+            os.system("cls")
+        else:
+            os.system("clear")
+
+        logo_settings()
+
+        if self.lang == 'ru':
+            settings_menu_ru()
+        else:
+            settings_menu_en()
+
+        option = input(Fore.MAGENTA + ' > ' + Fore.GREEN)
+
+        if option == '0':
+            from beast import BeastBomber
+            beast = BeastBomber()
+            beast.main()
+
+        elif option == '1':
+            update_proxies()
+
+            if self.lang == 'ru':
+                text = '\nГотово'
+            else:
+                text = '\nDone'
+
+            time.sleep(1)
+            self.settings_main()
+
+        elif option == '2':
+            change_language()
+            self.lang = get_lang()
+
+            if self.lang == 'ru':
+                text = '\nГотово'
+            else:
+                text = '\nDone'
+
+            time.sleep(1)
+            self.settings_main()
+
+        elif option == '3':
+            try:
+                os.remove(os.path.abspath('__pycache__'))
+            except:
+                pass
+            try:
+                os.remove(os.path.abspath('core/ddos_attack/__pycache__'))
+            except:
+                pass
+            try:
+                os.remove(os.path.abspath('core/discord_spam/__pycache__'))
+            except:
+                pass
+            try:
+                os.remove(os.path.abspath('core/email_spam/__pycache__'))
+            except:
+                pass
+            try:
+                os.remove(os.path.abspath('core/etc/__pycache__'))
+            except:
+                pass
+            try:
+                os.remove(os.path.abspath('core/sms_spam/__pycache__'))
+            except:
+                pass
+            try:
+                os.remove(os.path.abspath('core/telegram_spam/__pycache__'))
+            except:
+                pass
+
+            if self.lang == 'ru':
+                text = '\nГотово'
+            else:
+                text = '\nDone'
+
+            time.sleep(1)
+            self.settings_main()
+
+        else:
+            from beast import BeastBomber
+            beast = BeastBomber()
+            beast.ex()
